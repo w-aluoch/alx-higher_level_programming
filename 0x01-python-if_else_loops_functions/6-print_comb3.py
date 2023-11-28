9affd58eec91694e39b0358ffde9c38143908ac7
w-aluoch <wendyaluoch96@gmail.com>
2023-11-28 12:21:11 -0800

    loops_functions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Author - Osifo Goodness
+# Author - Wendy Aluoch
 
 for digit1 in range(0, 10):
     for digit2 in range(digit1 + 1, 10):
--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Author - Osifo Goodness
+# Author - Wendy Aluoch
 
 def islower(c):
     """Function checks for lowercase characters."""
