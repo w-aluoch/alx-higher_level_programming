709ede025b08d54eab8eea5903b5f76490cc26cc
w-aluoch <wendyaluoch96@gmail.com>
2023-11-30 12:55:44 -0800

    python imports

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+if _name_ == "_main_":
+
+    import sys
+
+    arg = sys.argv
+
+    size = len(arg) - 1
+
+    if size > 1:
+        print("{} arguments:".format(size))
+
+    for i in range(1, size + 1):
+            print("{}: {}".format(i, arg[i]))
+
+    elif size == 0:
+        print("{} arguments.".format(size))
+
+    else:
+        print("{} argument:".format(size))
+        print("{}: {}".format(size, arg[1]))
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+def add_arg(argv):
+
+    n = len(argv) - 1
+
+        if n == 0:
+            print("{:d}".format(n))
+
+        return
+
+        else:
+            i = 1
+
+            add = 0
+
+        while i <= n:
+        add += int(argv[i])
+                i += 1
+        print("{:d}".format(add))
+
+    if _name_ == "_main_":
+
+        import sys
+    add_arg(sys.argv)
