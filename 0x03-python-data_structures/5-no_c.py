8a622028d84dae2e9c4e9ce66c09e860b314416c
w-aluoch <wendyaluoch96@gmail.com>
2023-12-06 01:51:13 -0800

    python structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@
 def no_c(my_string):
     update_str = ''
     for i in my_string:
-        if i != 'c' and i !='C':
+        if i != 'c' and i != 'C':
             update_str += i
             return (update_str)
