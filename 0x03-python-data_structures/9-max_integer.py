1759a66f6eec9d6f97bbc224b81e667e819d9a8c
w-aluoch <wendyaluoch96@gmail.com>
2023-12-06 03:29:30 -0800

    python structures

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+
 def max_integer(my_list=[]):
-        new_list = []
-    if my_list:
-        my_list.sort(reverse=True)
-        return (my_list[0])
-    return (None)
+    if len(my_list) == 0:
+        return (None)
+    else:
+        max_int = my_list[0]
+        for i in range(len(my_list)):
+            if my_list[i] > max_int:
+                max_int = my_list[i]
+        return (max_int)
