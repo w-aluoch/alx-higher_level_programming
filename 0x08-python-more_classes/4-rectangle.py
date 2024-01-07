9701caecc4150ef2e027f77ef0334fbc9db49747
w-aluoch <wendyaluoch96@gmail.com>
2024-01-07 04:23:12 -0800

    more_classes

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -43,20 +43,20 @@ class Rectangle:
 
     def area(self):
         """Return the area of the Rectangle."""
-        return (self._width * self._height)
+        return (self.__width * self.__height)
 
     def perimeter(self):
         """Return the perimeter of the Rectangle."""
-        if self._width == 0 or self._height == 0:
+        if self.__width == 0 or self.__height == 0:
             return (0)
-        return ((self._width * 2) + (self._height * 2))
+        return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
         """Return the printable representation of the Rectangle.
 
         Represents the rectangle with the # character.
         """
-        if self._width == 0 or self._height == 0:
+        if self.__width == 0 or self.__height == 0:
             return ("")
 
         rect = []
